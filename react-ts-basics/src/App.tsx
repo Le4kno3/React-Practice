c4ff974576ee283a8af2d7872e0c68df4c8170a8
le4kno3 <jlelo403@gmail.com>
2022-06-04 18:19:01 +0530

    initial commit

---
+++ react-ts-basics/src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./App.css";
+
+let name: string;
+let age: number;
+
+function App() {
+  return <div className="App">Hello World</div>;
+}
+
+export default App;
