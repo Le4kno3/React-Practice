9173ef166cde286c72c3c707722bcc1c40b8cd6e
le4kno3 <le4kno3@gmail.com>
2023-02-15 18:40:23 +0530

    useEffects and useRefs examples

--- react-hooks-tutorials/src/components/useEffects/for_synchronization/Chat.js
+++ react-hooks-tutorials/src/components/useEffects/for_synchronization/Chat.js
@@ -1,13 +1,14 @@
 export function createConnection(serverUrl, roomId) {
   // A real implementation would actually connect to the server
+  // remove the "StrictMode" of React Development environment to get the correct results, it is generally in "src/index.tsx" file.
   return {
     connect() {
-      // console.log(
-      //   '✅ Connecting to "' + roomId + '" room at ' + serverUrl + '...'
-      // )
+      console.log(
+        '✅ Connecting to "' + roomId + '" room at ' + serverUrl + '...'
+      )
     },
     disconnect() {
-      // console.log('❌ Disconnected from "' + roomId + '" room at ' + serverUrl)
+      console.log('❌ Disconnected from "' + roomId + '" room at ' + serverUrl)
     }
   }
 }
